dbe892b9c9c5c1a0a509df95de78e85ab8750ee4
Xsidz <siddhesh.kabraa@gmail.com>
2025-09-09 17:20:28 +0530

    refactor

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,6 +4,7 @@ import { gennToken } from "../utils/lib.js";
 export const signUp = async (req, res) => {
   const { Name, Email, Password, Address } = req.body;
   try {
+    // validate values from imputs
     if (!Name || !Email || !Password || !Address) {
       return res.status(400).json({ message: "All fields are required !! " });
     }
@@ -12,6 +13,8 @@ export const signUp = async (req, res) => {
         .status(400)
         .json({ message: "Password Length Should be atleast 6 Characters" });
     }
+
+    // check for existing user
     const pool = getPool();
     const [existingUser] = await pool.query(
       `SELECT * FROM users WHERE email = ?`,
@@ -20,6 +23,8 @@ export const signUp = async (req, res) => {
     if (existingUser.length > 0) {
       return res.status(400).json({ message: "User Already Exists" });
     }
+
+    //hash password
     const salt = await bcrypt.genSalt(10);
     const hashedPass = await bcrypt.hash(Password, salt);
 
@@ -31,11 +36,13 @@ export const signUp = async (req, res) => {
     console.log(result);
 
     const [rows] = await pool.query(
-      `SELECT id,name, email, address, role, created_at,updated_at FROM users WHERE id = ?`, [result.insertId]
+      `SELECT id,name, email, address, role, created_at,updated_at FROM users WHERE id = ?`,
+      [result.insertId]
     );
 
+    // geneerate jwt-token to start session
     const newUser = rows[0];
-    console.log(newUser.id)
+    console.log(newUser.id);
 
     gennToken(newUser.id, res);
 
@@ -45,9 +52,10 @@ export const signUp = async (req, res) => {
     return res.status(500).json({ message: "Internal Server Error!!" });
   }
 };
-export const logIn = async(req, res) => {
+export const logIn = async (req, res) => {
   const { Email, Password } = req.body;
   try {
+    //validate the fields
     if (!Email || !Password) {
       return res.status(400).json({ message: "All fields are required!!" });
     }
@@ -61,6 +69,7 @@ export const logIn = async(req, res) => {
     if (!user) {
       return res.status(400).json({ message: "Inavlid Credentials" });
     }
+    //comapre the user entered and stored password
     const isPassCorrect = await bcrypt.compare(Password, user.password);
     if (!isPassCorrect) {
       return res.status(400).json({ message: "Inavlid Credentials" });
@@ -83,8 +92,14 @@ export const logIn = async(req, res) => {
 };
 export const logOut = (req, res) => {
   try {
-    res.cookie("jwt", "", {
-      maxAge: 0,
+    if (!req.cookies.jwt) {
+      return res.status(200).json({ message: "Already Logged Out" });
+    }
+
+    res.clearCookie("jwt", {
+      httpOnly: true,  
+      secure: process.env.NODE_ENV !== "development", 
+      sameSite: "strict"
     });
 
     return res.status(200).json({ message: "LoggedOut Successfully" });
@@ -94,29 +109,38 @@ export const logOut = (req, res) => {
   }
 };
 
-export const updatePassword = async()=>{
-    const{currentPassword, newPassword} = req.body;
+export const updatePassword = async () => {
+  const { currentPassword, newPassword } = req.body;
 
-    try {
-        const pool = getPool()
-        const [row] = await pool.query(`SELECT password FROM users WHERE id =?`,[req.user.id])
-        if(row.lenght == 0) return res.status(404).json({message : "User not Found!!"})
-        const user = row[0];
-        const passwordMatch = await bcrypt.compare(currentPassword,user.password)
-        if(!passwordMatch) return res.status(400).json({message : "Current password is incorrect!!"})
-        const salt = await bcrypt.genSalt(10);
-        const newHashed = await bcrypt.hash(newPassword,salt);
+  try {
+    const pool = getPool();
+    const [row] = await pool.query(`SELECT password FROM users WHERE id =?`, [
+      req.user.id,
+    ]);
+    if (row.lenght == 0)
+      return res.status(404).json({ message: "User not Found!!" });
+    const user = row[0];
+    const passwordMatch = await bcrypt.compare(currentPassword, user.password);
+    if (!passwordMatch)
+      return res
+        .status(400)
+        .json({ message: "Current password is incorrect!!" });
+    const salt = await bcrypt.genSalt(10);
+    const newHashed = await bcrypt.hash(newPassword, salt);
 
-        await pool.query(`UPDATE users SET paswword = ? WHERE id = ?`,[newHashed,req.user.id])
+    await pool.query(`UPDATE users SET paswword = ? WHERE id = ?`, [
+      newHashed,
+      req.user.id,
+    ]);
 
-        console.log("Password Updated");
-        res.clearCookie("jwt");
+    console.log("Password Updated");
+    res.clearCookie("jwt");
 
-        return res.status(200).json({message:"Password updated successfully. Please log in again"})
-
-    } catch (error) {
-        console.log("Error in the udpatePassword Controller : ", error.message)
-        return res.status(500).json({message:"Internal Server Error"})
-    }
-}
-
+    return res
+      .status(200)
+      .json({ message: "Password updated successfully. Please log in again" });
+  } catch (error) {
+    console.log("Error in the udpatePassword Controller : ", error.message);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
