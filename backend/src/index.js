0a945a24b0b278cc598e2963076d5d096361b2fb
Xsidz <siddhesh.kabraa@gmail.com>
2025-09-11 00:54:06 +0530

    ..

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import authRoute from "./route/auth.route.js";
-import adminRoute from "./route/user.route.js"
-import storeRoutes from "./route/store.route.js"
-import { connectDB } from "./utils/connectDB.js";
+
+import authRoutes from "./route/auth.route.js";
+import userRoutes from "./route/user.route.js";
+import storeRoutes from "./route/store.route.js";
 import ratingRoutes from "./route/rating.route.js";
+import dashboardRoutes from "./route/dashboard.route.js";
+
+import { connectDB } from "./utils/connectDB.js";
 
 dotenv.config();
 const app = express();
@@ -15,22 +18,24 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use(
   cors({
-    origin: "http://localhost:5500",
+    origin: "http://localhost:5500", // frontend origin
     methods: "GET, POST, PUT, DELETE, OPTIONS",
     allowedHeaders: "Content-Type, Authorization",
     credentials: true,
   })
 );
 
-app.use("/auth", authRoute);
-app.use("/api/users", adminRoute)
-app.use("/",storeRoutes)
+// Routes
+app.use("/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/stores", storeRoutes);
 app.use("/api/ratings", ratingRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 
-app.listen(PORT, (req, res) => {
-  console.log("Server active on Port" + " " + PORT);
+app.listen(PORT, () => {
+  console.log(`Server active on Port ${PORT}`);
   connectDB();
 });
-
--- backend/src/route/dashboard.route.js
+++ backend/src/route/dashboard.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
 import { requireAuth } from "../middlewares/auth.middleware.js";
+import { getDashboard } from "../controllers/dashboard.controller.js";
 
-router.get("/", requireAuth(["admin"]), dashboardStats);
+router.get("/", requireAuth(["admin"]), getDashboard);
 
 
 export default router;
